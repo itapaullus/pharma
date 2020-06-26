bb3de978620eee88acee3f44584d6291c85cb619
itapaullus <eww.paullus@gmail.com>
2020-06-26 22:30:38 +0300

    Добавили регионы

---
+++ db_class.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+gid = 0
+class Region:
+    def __init__(self, label, args):
+        self.label = label
+        self.synonyms = set(list(args) + [label])
+
+class Regions(list):
+    def __init__(self, path=None):
+        super(Regions, self).__init__()
+        df = pd.read_excel(path)
+        for i in df.to_records():
+            self.append(Region(i[1], list(i)[1:]))
+
+class DBRegion:
+    @staticmethod
+    def get(label):
+        # лезем в табоицу и возвращаем ID
+        return None
+
+    @staticmethod
+    def save_synonyms(id, synonyms):
+        print(f'Сохранили {id} {synonyms}')
+
+    @staticmethod
+    def save_region(label):
+        global gid
+        gid += 1
+        print(f'Сохранили регион {gid} - {label}')
+        return gid
+
+    @staticmethod
+    def getlist():
+        return Regions()
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print('Hello world')
-print('kim features branch')
+import pandas as pd
+import db_class as db
+
+RegList = db.Regions('Regions.xlsx')
+for i in RegList:
+    # print(i.label)
+    # print(i.synonyms)
+    regionid = db.DBRegion.get(i.label)
+    if regionid:
+        db.DBRegion.save_synonyms(regionid, i.synonyms)
+    else:
+        newid = db.DBRegion.save_region(i.label)
+        db.DBRegion.save_synonyms(newid, i.synonyms)
+
+
+
+# reg = Region('Адыгея', ['Адыгея', 'Адыгейская адыгея'])
+# print(reg.label)
+# print(reg.synonyms)
