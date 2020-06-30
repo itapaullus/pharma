58f2d9dad53d4415dcceadab8e8a9c2ce059ef5c
itapaullus <tax_norm_f135>
2020-06-30 19:54:33 +0300

    Добавил класс для общения с Dadata для получения клиента по ИНН

---
+++ venv/ext_api.py
@@ -0,0 +1,24 @@
+import requests as r
+import json
+
+
+class Dadata:
+    _TOKEN = 'Token e2f762391c5d378c95cbacfc255d4a4dd696b3a7'
+    _URL_INN = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party'
+    @classmethod
+    def get_client_by_inn(cls, inn):
+        resp = r.post(cls._URL_INN,
+                    data=json.dumps({
+                        "query": inn
+                    }),
+                    headers={
+                        'Authorization': cls._TOKEN,
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                    )
+        return resp.json()
+
+
+request = Dadata.get_client_by_inn("7707083893")
+print(request)
